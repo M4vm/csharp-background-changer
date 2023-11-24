7d4527d41e52f7be292131633cbcdbc27a374788
M4vm <m4vm.gw@gmail.com>
2023-11-24 01:30:10 +0100

    init

---
+++ ChangeBG/Form1.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Runtime.InteropServices;
+using System.Runtime.InteropServices.ComTypes;
+using System.Net;
+using System.IO;
+
+namespace ChangeBG
+{
+    public partial class Form1 : Form
+    {
+
+        public const int SPI_SETDESKWALLPAPER = 20;
+        public const int SPIF_UPDATEINIFILE = 1;
+        public const int SPIF_SENDCHANGE = 2;
+
+        [DllImport("user32.dll", CharSet = CharSet.Auto, SetLastError = true)]
+        public static extern int SystemParametersInfo(int uAction, int uParam, string lpvParam, int fuWinIni);
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Change()
+        {
+            var path = Path.Combine(Directory.GetCurrentDirectory(), "linus.bmp");
+            SystemParametersInfo(SPI_SETDESKWALLPAPER, 0, path, SPIF_UPDATEINIFILE | SPIF_SENDCHANGE);
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Change();
+        }
+    }
+}
